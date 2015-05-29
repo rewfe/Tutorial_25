4bf9bc4689af79fe07b3a6e48f60d668097e4595
rewfe <qrewfe@yandex.ru>
2015-05-30 02:41:41 +0300

    Initial Commit

---
+++ Tutorial_25/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutorial_25
+//
+//  Created by Admin on 30.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
