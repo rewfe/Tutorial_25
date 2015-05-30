8d034dfc325bbe7682adce5d7498ccc1e515639a
rewfe <qrewfe@yandex.ru>
2015-05-30 04:17:10 +0300

    prepare Storyboard

---
+++ Tutorial_25/AddToDoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToDoViewController.h
+//  Tutorial_25
+//
+//  Created by Admin on 30.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToDoViewController : UIViewController
+
+@end
---
+++ Tutorial_25/ToDoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListViewController.h
+//  Tutorial_25
+//
+//  Created by Admin on 30.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListViewController : UITableViewController
+
+@end
